de93b9f03d9fa605a7143a03c564a778765aaa89
lenny <lennyzepho92@gmail.com>
2026-04-17 10:11:43 +0200

    Ajout de la page Politique de confidentialité avec contenu

---
+++ src/Controller/PrivacyController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class PrivacyController extends AbstractController
+{
+    #[Route('/Politique de confidentialité', name: 'app_privacy')]
+    public function index(): Response
+    {
+        return $this->render('privacy/index.html.twig', [
+            
+        ]);
+    }
+}
